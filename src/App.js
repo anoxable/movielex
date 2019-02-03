3fcef83118f3e817ec8ebfd846de1980de9f70b8
Nemanja <anoxable@gmail.com>
2019-02-03 18:06:25 +0100

    first commit,started styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Search from './Components/Search'
+import './App.css'
+import axios from 'axios'
+import Movie from './Components/Movie'
+import Sort from './Components/Sort'
 
 class App extends Component {
+  state = {
+    movies: [],
+    genres: [],
+    director: [],
+    cast: []
+
+  }
+  componentDidMount() {
+    axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3327de82c58bbb5e77470af2495090c6&language=en-US').then(res => {
+      console.log(res)
+      this.setState({
+        movies: res.data.results.slice(0, 18)
+      })
+
+    })
+  }
+  handleUpdateMovie = (movies) => {
+    this.setState({
+      movies: movies
+    })
+
+  }
+  handleHome = () => {
+    this.componentDidMount()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>Movie-App</h1>
+        <Search UpdateMovie={this.handleUpdateMovie} />
+        <button onClick={this.handleHome}>Home</button>
+        <Sort UpdateMovie={this.handleUpdateMovie} />
+        <Movie state={this.state} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Components/Movie.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import '../App.css'
+import axios from 'axios'
+
+class Movie extends Component {
+  state = {
+    genres: [],
+    director: [],
+    cast: []
+  }
+
+  /* query for each movie to get more in depth details */
+
+
+
+  render() {
+
+    const { movies } = this.props.state
+    console.log(movies)
+
+    let movie = movies.map((movie, index) => {
+
+      if (movie.poster_path == null) {
+        return (null)
+      } else {
+        let url = 'https://image.tmdb.org/t/p/w185/' + movie.poster_path
+        return (
+          <div key={index}>
+            <div className='movie'>
+              <img className='poster' src={url} />
+              <div className="movie-info">
+                <span className='movie-rating'>{movie.vote_average}</span>
+                <h1 className='movie-title'>{movie.title}</h1>
+                <p className='movie-release'>{movie.release_date}</p>
+                <p className='movie-overview'>{movie.overview}</p>
+              </div>
+            </div>
+          </div >
+        )
+      }
+    })
+    console.log(movies)
+    return (
+      <div className='wrapp'>
+        {movie}
+      </div>
+    )
+  }
+}
+export default Movie
